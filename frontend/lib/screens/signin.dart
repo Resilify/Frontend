b64368d07bd66d8e4c923ac657183c499176ff5f
Kaveesh Tennakoon <kaveeshoffical@gmial.com>
2025-01-02 00:27:21 +0530

    sign in'

--- frontend/lib/screens/signin.dart
+++ frontend/lib/screens/signin.dart
@@ -1,10 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/core/constants/app_colors.dart';
+import 'package:frontend/widgets/custom_button.dart';
+import 'package:frontend/widgets/text_field.dart';
 
 class Signin extends StatelessWidget {
-  const Signin({super.key});
+  Signin({super.key});
+
+  final _formkey = GlobalKey<FormState>();
+
+  void signin(context) {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
+      print(userNameController.text);
+      print(passwordController.text);
+
+      // Navigator.pushReplacementNamed(context, '/chat',
+      //     arguments: userNameController.text);
+
+      print("Login Presed");
+    } else {
+      print("Login unsuccessful");
+    }
+  }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Signin");
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Column(
+        children: [
+          // above section and image
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Center(
+              child: Image.asset('assets/img/logo_2.png'),
+            ),
+          ),
+
+          //rest of the screen components
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: AppColors.secondaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30, top: 50, right: 30),
+
+                // title and rest of the details in coloum format
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      "Sign in",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primaryTextColor),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+
+                    // textfields
+                    Form(
+                      key: _formkey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // text
+                          Text(
+                            "Email",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primaryTextColor,
+                            ),
+                          ),
+
+                          // input feild
+                          CustomTextField(
+                            hintText: "Enter email",
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter your email";
+                              } else if (!RegExp(
+                                      r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                                  .hasMatch(value)) {
+                                return "Please enter a valid email address";
+                              }
+                              return null;
+                            },
+                            controller: userNameController,
+                          ),
+
+                          SizedBox(
+                            height: 15,
+                          ),
+
+                          // text
+                          Text(
+                            "Password",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primaryTextColor,
+                            ),
+                          ),
+
+                          // input feild
+                          CustomTextField(
+                              controller: passwordController,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please enter your password";
+                                }
+                                return null;
+                              },
+                              hintText: "Enter password",
+                              hasAsteriks: true),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          child: Text("Forgot password?"),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    CustomButton(
+                        text: "Sign in",
+                        onPress: () {
+                          signin(context);
+                        })
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ frontend/lib/widgets/text_field.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/core/constants/app_colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final FormFieldValidator<String>? validator;
+  final bool hasAsteriks;
+
+  const CustomTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      this.validator,
+      this.hasAsteriks = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        validator: (value) {
+          if (validator != null) {
+            return validator!(value);
+          }
+        },
+        controller: controller,
+        obscureText: hasAsteriks,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(color: AppColors.primaryColor),
+          filled: true,
+          fillColor: Color(0xFFF3F4F6),
+          contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: AppColors.primaryColor, width: 1.5)
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          )
+        )
+      );
+  }
+}
