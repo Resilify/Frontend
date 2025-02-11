146691a5fd951c3c709494c7dfc8a4cc5fde7c84
Nilni <nilni.divyanjali@gmail.com>
2025-02-11 12:33:22 +0530

    Saved local changes before pulling latest updates

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/core/constants/app_colors.dart';
+import 'package:frontend/screens/erp_loop.dart';
 import 'package:frontend/screens/home.dart';
 import 'package:frontend/screens/landing.dart';
 import 'package:frontend/screens/signin.dart';
 import 'package:frontend/screens/signup.dart';
 import 'package:frontend/screens/splash.dart';
-import 'package:frontend/screens/erp_loop.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
